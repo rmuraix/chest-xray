5e62c5bab5e79dc390aaac03141b0f75b7551d20
Ryota Murai <35632215+rmuraix@users.noreply.github.com>
2025-02-20 03:18:43 +0000

    feat: add densenet121

---
+++ models/densenet.py
@@ -0,0 +1,21 @@
+import torch.nn as nn
+from torchvision import models
+
+
+class DenseNetMultiLabel(nn.Module):
+    """
+    CNN model based on DenseNet121 for multi-label classification tasks.
+
+    Args:
+        num_classes (int): Number of output classes.
+    """
+
+    def __init__(self, num_classes: int, weights: str = "DEFAULT"):
+        super(DenseNetMultiLabel, self).__init__()
+        self.densenet = models.densenet121(weights="DEFAULT")
+        num_features = self.densenet.classifier.in_features
+        self.densenet.classifier = nn.Sequential(nn.Linear(num_features, num_classes))
+
+    def forward(self, x):
+        x = self.densenet(x)
+        return x
